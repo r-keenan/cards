28560b6a7bed495b890e8f0a77944c52554fe957
rkeenan <ross.keenan@protonmail.comm>
2023-06-25 15:54:10 -0500

    Created newDeck for creating a new deck.

--- deck.go
+++ deck.go
@@ -6,6 +6,22 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+// no receiver, because we are calling this to get a new deck and not calling it on an existing deck
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	
+	// use '_' to replace the index variable, because you do not need to use it in these loops
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit )
+		}
+	}
+
+	return cards
+}
+
 // (d deck) is the receiver for the function
 // any variable of type 'deck' gets access to the printDeck() method
 func (d deck) printDeck() {
@@ -13,3 +29,4 @@ func (d deck) printDeck() {
 		fmt.Println(i, card)
 	}
 }
+
--- main.go
+++ main.go
@@ -4,8 +4,7 @@ func main() {
 	//both of these lines accomplish the same thing
 	//var card string = "Ace of Spades"
 	//this is a slice, and all data types in the slice must be the same.
-	cards := deck{"Ace of Diamonds", newCard()}	
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.printDeck()
 }
