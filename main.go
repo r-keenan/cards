96bf8d7cfb4177ab56368d033faed3899c89c3d4
rkeenan <ross.keenan@protonmail.comm>
2023-06-25 10:50:42 -0500

    Covering both way to declare a variable.

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//both of these lines accomplish the same thing
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	
+	fmt.Println(card)
+}
+
