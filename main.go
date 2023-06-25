59ed208f531e4aa426131a3a2af3eff67d091dd8
rkeenan <ross.keenan@protonmail.comm>
2023-06-25 15:10:07 -0500

    Looping through a slice and printing out each line.

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import "fmt"
 func main() {
 	//both of these lines accomplish the same thing
 	//var card string = "Ace of Spades"
-	card := newCard()
-	
-	fmt.Println(card)
+	//this is a slice, and all data types in the slice must be the same.
+	cards := []string{"Ace of Diamonds", newCard()}	
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
