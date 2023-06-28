f7ce87156a537b933cc2334b483c17060b2fa7c6
rkeenan <ross.keenan@protonmail.comm>
2023-06-27 19:09:17 -0500

    Added new functions for turning a deck into a string and saving a file to the local hard drive.

--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 func main() {
 	//both of these lines accomplish the same thing
 	//var card string = "Ace of Spades"
+	// cards := "Ace of Spades"
 	//this is a slice, and all data types in the slice must be the same.
 	cards := newDeck()
-
-	// first return value assigned to hand; second return value assigned to remainingCards
-	hand, remainingCards := deal(cards, 5)
-
-	hand.printDeck()
-	remainingCards.printDeck()
+	cards.saveToFile("my_cards.txt")
 }
