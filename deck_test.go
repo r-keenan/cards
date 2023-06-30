bc755af143c3ede99bc3205bf0e4f42b91096482
rkeenan <ross.keenan@protonmail.comm>
2023-06-30 15:17:32 -0500

    Added new test for saveToDeck and newDeckFromFile functions.

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "King of Clubs" {
 		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d) - 1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting.txt")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting.txt")
+
+	loadedDeck := newDeckFromFile("_decktesting.txt")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting.txt")
 }
